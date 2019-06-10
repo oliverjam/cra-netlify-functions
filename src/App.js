d53b76b832e40cad74a352cdd42b713acd387189
oliver.phillips <oliver.phillips@ticketmaster.co.uk>
2019-06-10 13:50:21 +0100

    Use example lambda in React app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 function App() {
-  return <div>Hello world</div>;
+  const [data, setData] = React.useState(null);
+  React.useEffect(() => {
+    fetch("/.netlify/functions/getGithubData")
+      .then(res => {
+        if (!res.ok) throw new Error("HTTP error");
+        return res.json();
+      })
+      .then(data => setData(data));
+  }, []);
+  if (!data) return <div>Loading...</div>;
+  return <pre>{JSON.stringify(data, null, 2)}</pre>;
 }
 
 export default App;
